ffdce20e36b8e9c01306971f7b4cd29c2240a6ab
Unknown <greben19900516@gmail.com>
2017-12-08 20:10:03 +0300

    Auto stash before merge of "master" and "origin/master"
    добавлены views в проект

---
+++ view/v_index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Шаблон главной страницы
+ * =======================
+ * $text - текст
+ */
+?>
+
+<?/*=nl2br($text)*/?>
+
+<div class="row">
+
+  <div class="col-sm-12">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Special title treatment</h4>
+        <p class="card-text"><?=nl2br($text)?></p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+
+
+</div>
